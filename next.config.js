35c8ef3deb4aa9049a8c05947359a502bdd9b8e9
lokeshKumar <lokeshkumargithub@gmail.com>
2022-11-13 01:13:50 +0530

    next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors:true,
+  },
+  
   reactStrictMode: true,
   swcMinify: true,
+
   images: {
     domains: [
       'lh3.googleusercontent.com',
