bb021db7267b017697ac2cb2bfa9a48f5adb5c6b
lokeshKumar <lokeshkumargithub@gmail.com>
2022-11-12 23:29:38 +0530

    SIDEBAR NAVBAR COMP

--- utils/client.ts
+++ utils/client.ts
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client';
 
 export const client = sanityClient({
-  projectId: 'tp4ln3j8',
+  projectId: 'ev683rbw',
   dataset: 'production',
   apiVersion: '2022-03-10',
   useCdn: false,
